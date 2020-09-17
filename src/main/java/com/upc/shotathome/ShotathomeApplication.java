9548c594a07950efd7f2d492b9782fd5bea1dc92
Lenovo <christian_joseph7@hotmail.com>
2020-09-16 23:47:06 -0500

    Initial commit

---
+++ src/main/java/com/upc/shotathome/ShotathomeApplication.java
@@ -0,0 +1,13 @@
+package com.upc.shotathome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShotathomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShotathomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/shotathome/ShotathomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.shotathome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShotathomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
